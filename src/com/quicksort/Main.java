18ab56e0c20ec8ded5fc506f5d0764768bf1640e
MBaran88 <zdzibq@gmail.com>
2017-05-22 17:34:59 +0200

    quicksort

---
+++ src/com/quicksort/Main.java
@@ -0,0 +1,30 @@
+package com.quicksort;
+
+public class Main {
+
+    public static void main(String[] args) {
+	class Person{
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        private int age;
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        private String surname;
+
+
+    }
+    }
+}
